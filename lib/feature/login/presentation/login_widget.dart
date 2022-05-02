30cb79a2fa90fed20d3bb009b38dbd83ee4c1db5
Andrea <andrea.andrea@gopay.co.id>
2022-05-02 16:37:27 +0800

    Add retrofit

--- lib/feature/login/presentation/login_widget.dart
+++ lib/feature/login/presentation/login_widget.dart
@@ -1,11 +1,20 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:secret_hitler/common/style/style_normal_text.dart';
+import 'package:secret_hitler/network/network_service.dart';
 
 class LoginWidget extends StatelessWidget {
   const LoginWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final network = NetworkService(Dio(), baseUrl: 'https://gorest.co.in/public-api/');
+    network.getUsers().then((value) {
+      print('hahaha $value');
+    }).onError((error, stackTrace) {
+      print('hahaha error $error');
+    });
+
     return Column(
       children: const [
         Text(
--- lib/network/network_service.dart
+++ lib/network/network_service.dart
@@ -1,1 +1,12 @@
-class NetworkService {}
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'network_service.g.dart';
+
+@RestApi()
+abstract class NetworkService {
+  factory NetworkService(Dio dio, {String baseUrl}) = _NetworkService;
+
+  @GET('/users')
+  Future<String> getUsers();
+}
