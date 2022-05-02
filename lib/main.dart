a76fa9658730d175b3f052668adcbd5c4ad2e41c
Andrea <andrea.andrea@gopay.co.id>
2022-05-02 14:50:42 +0800

    Add Get It as Dependency Injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:secret_hitler/di/get_it.dart';
 import 'package:secret_hitler/secret_hitler_app.dart';
 
 void main() {
+  configureDependencies();
   runApp(const SecretHitlerApp());
 }
