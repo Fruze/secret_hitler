8b4685cc6236b833ce388d7cf705c1eb37d4c806
Andrea <andrea.andrea@gopay.co.id>
2022-05-04 21:11:35 +0800

    Trial

--- lib/feature/login/presentation/login_widget.dart
+++ lib/feature/login/presentation/login_widget.dart
@@ -1,15 +1,69 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:secret_hitler/common/style/style_normal_text.dart';
 import 'package:secret_hitler/di/get_it.dart';
 import 'package:secret_hitler/feature/login/presentation/login_cubit.dart';
 import 'package:secret_hitler/feature/login/presentation/login_state.dart';
+import 'package:stomp_dart_client/stomp.dart';
+import 'package:stomp_dart_client/stomp_config.dart';
+import 'package:stomp_dart_client/stomp_frame.dart';
 
-class LoginWidget extends StatelessWidget {
+class LoginWidget extends StatefulWidget {
   const LoginWidget({Key? key}) : super(key: key);
 
   @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  StompClient? client;
+
+  void onConnectCallback(StompFrame connectFrame) {
+    print('hahaha in');
+
+    client?.send(
+      destination: '/topic/messages',
+      body: '{"from":"","text":"andrea"}',
+      headers: {},
+    );
+
+    client?.subscribe(
+      destination: '/topic/messages/andrea',
+      headers: {},
+      callback: (frame) {
+        // Received a frame for this subscription
+        print('hahaha ads');
+        print(frame.body);
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    client = StompClient(
+      config: StompConfig(
+        url: 'ws://stomp.herokuapp.com/returning',
+        onConnect: onConnectCallback,
+        beforeConnect: () async {
+          print('hahaha before connect');
+        },
+        onStompError: (stompFrame) {
+          print('hahaha error $stompFrame');
+        },
+        onDebugMessage: (stompFrame) {
+          print('hahaha debug $stompFrame');
+        },
+        onWebSocketError: (stompFrame) {
+          print('hahaha ws error $stompFrame');
+        },
+      ),
+    );
+
+    client?.activate();
+    client?.ack(id: 'asd');
+
     return BlocProvider(
       create: (context) => getIt<LoginCubit>()..getNames(),
       child: BlocBuilder<LoginCubit, LoginState>(
