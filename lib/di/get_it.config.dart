7fd266025d2cd9b1dc9be3c80aec7dbb0871e7f0
Andrea <andrea.laukaban@gmail.com>
2022-08-06 17:11:44 +0700

    Add home cubit and usecase

--- lib/di/get_it.config.dart
+++ lib/di/get_it.config.dart
@@ -8,10 +8,12 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../feature/home/data/home_repository.dart' as _i4;
-import '../feature/login/data/login_repository.dart' as _i5;
-import '../feature/login/domain/login_usecase.dart' as _i6;
-import '../feature/login/presentation/login_cubit.dart' as _i7;
-import '../network/network_module.dart' as _i8;
+import '../feature/home/domain/home_usecase.dart' as _i5;
+import '../feature/home/presentation/home_cubit.dart' as _i8;
+import '../feature/login/data/login_repository.dart' as _i6;
+import '../feature/login/domain/login_usecase.dart' as _i7;
+import '../feature/login/presentation/login_cubit.dart' as _i9;
+import '../network/network_module.dart' as _i10;
 import '../network/network_service.dart'
     as _i3; // ignore_for_file: unnecessary_lambdas
 
@@ -24,12 +26,15 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.factory<_i3.NetworkService>(() => networkModule.networkService);
   gh.factory<_i4.HomeRepository>(
       () => _i4.HomeRepositoryImpl(get<_i3.NetworkService>()));
-  gh.factory<_i5.LoginRepository>(
-      () => _i5.LoginRepositoryImpl(get<_i3.NetworkService>()));
-  gh.factory<_i6.LoginUseCase>(
-      () => _i6.LoginUseCaseImpl(get<_i5.LoginRepository>()));
-  gh.factory<_i7.LoginCubit>(() => _i7.LoginCubit(get<_i6.LoginUseCase>()));
+  gh.factory<_i5.HomeUseCase>(
+      () => _i5.HomeUseCaseImpl(get<_i4.HomeRepository>()));
+  gh.factory<_i6.LoginRepository>(
+      () => _i6.LoginRepositoryImpl(get<_i3.NetworkService>()));
+  gh.factory<_i7.LoginUseCase>(
+      () => _i7.LoginUseCaseImpl(get<_i6.LoginRepository>()));
+  gh.factory<_i8.HomeCubit>(() => _i8.HomeCubit(get<_i5.HomeUseCase>()));
+  gh.factory<_i9.LoginCubit>(() => _i9.LoginCubit(get<_i7.LoginUseCase>()));
   return get;
 }
 
-class _$NetworkModule extends _i8.NetworkModule {}
+class _$NetworkModule extends _i10.NetworkModule {}
